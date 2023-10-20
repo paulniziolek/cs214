cacd8e5f319d562279c99a5c26cc097c6f488f4c
paulniziolek <niziolek.paul@gmail.com>
2023-10-19 22:10:19 -0400

    Yes.

--- malloc/src/mymalloc.c
+++ malloc/src/mymalloc.c
@@ -3,15 +3,56 @@
 double heap[HEAP_SIZE_IN_BLOCKS];
 
 void *mymalloc_wrapper(size_t size, char *file, int line) {
-    // perform error checking here
-
-    return mymalloc(size);
+    //checking is size makes sense
+    if(size <= 0){
+        fprintf(stderr,"malloc error: size <= 0\n file: %s\n line: %d\n",file,line);
+        return NULL;
+    }
+    if(size > HEAP_SIZE){
+        fprintf(stderr,"malloc error: size > HEAP_SIZE_IN_BYTES\n file: %s\n line: %d\n",file,line);
+        return NULL;
+    }
+    void *ptr = mymalloc(size);
+    if(ptr == NULL){
+        fprintf(stderr,"malloc error: no space found\n file: %s\n line: %d\n",file,line);
+        return NULL;
+    }
+    return ptr;
 }
 
 void myfree_wrapper(void *ptr, char *file, int line) {
-    // perform error checking here
+    //check if ptr is in range
+    if((char*)ptr < (char*)&heap[0] || (char*)ptr >= (char*)END){
+        fprintf(stderr,"free error: ptr not in range\n file: %s\n line: %d\n",file,line);
+        return;        
+    }
+    //heap not initialized edge case
+    if(heap[0]==0){
+        fprintf(stderr,"free error: heap not initialized yet\n file: %s\n line: %d\n",file,line);
+        return;        
+    }
 
-    myfree(ptr);
+    //loop over current blocks
+    header_t *header = (header_t*)&heap[0];
+    header_t *need = (header_t*)ptr - HEADER_SIZE_IN_BLOCKS;
+    while(header != END){
+        if(header==need){
+            //already freed this block
+            if(header->isFree){
+                fprintf(stderr,"free error: ptr already freed\n file: %s\n line: %d\n",file,line);
+                return;
+            }
+            //block is valid, go ahead and free it
+            myfree(ptr);
+            return;
+        }
+        header=nextHeader(header);
+    }
+
+    //ptr not found
+    //either it was very old and block structure had changed
+    //or it was never returned by malloc in the first place
+    fprintf(stderr,"free error: ptr not found\n file: %s\n line: %d\n",file,line);
 }
 
 
