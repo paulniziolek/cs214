3c5c09b984da73dab4e0c60e464fa18c4f716758
paulniziolek <niziolek.paul@gmail.com>
2023-11-03 21:14:43 -0400

    Handled hashmap collisions with linked list

--- posix/src/nickmap.c
+++ posix/src/nickmap.c
@@ -1,7 +1,6 @@
 #include "nickmap.h"
-#include <stdio.h>
 
-int map[SIZE];
+pair_t* map[SIZE];
 ll M =   29996224275833; //rly big prime
 
 ll hash(char *str){
@@ -14,13 +13,38 @@ ll hash(char *str){
 }
 
 int map_get(char *str){
-    return map[hash(str)%SIZE];
+    pair_t* head = traverse_bucket(hash(str)%SIZE, str);
+    return head->value;
 }
 
 void map_set(char *str, int x){
-    map[hash(str)%SIZE] = x;
+    pair_t* head = traverse_bucket(hash(str)%SIZE, str);
+    head->value = x;
 }
 
 void map_inc(char *str){
-    map[hash(str)%SIZE]++;
+    pair_t* head = traverse_bucket(hash(str)%SIZE, str);
+    head->value++;
+}
+
+pair_t* traverse_bucket(ll bucket, char *str) {
+    // initialize bucket
+    if (map[bucket] == NULL) {
+        map[bucket] = (pair_t *)malloc(sizeof(pair_t));
+        map[bucket]->key = strdup(str);
+        map[bucket]->value = 0;
+        return map[bucket];
+    }
+    pair_t* head = map[bucket];
+    while (strcmp(head->key, str) != 0) {
+        if (head->next == NULL) {
+            // add str pair to bucket
+            head->next = (pair_t *)malloc(sizeof(pair_t));
+            head->next->key = strdup(str);
+            head->next->value = 0;
+            return head->next;
+        }
+        head = head->next;
+    }
+    return head;
 }
--- posix/src/word.c
+++ posix/src/word.c
@@ -1,16 +1,43 @@
 #include <stdio.h>
+#include <assert.h>
 #include "nickmap.h"
 
+long long global_ctr = 1;
+
+void generate_strings(char *str, int index, int length) {
+    if (index == length) {
+        str[index] = '\0'; 
+
+        assert(map_get(str) == 0);
+        map_set(str, global_ctr);
+        global_ctr++;
+        printf("%lld\n", global_ctr);
+        return;
+    }
+
+    for (char c = 'a'; c <= 'z'; c++) {
+        str[index] = c;
+        generate_strings(str, index + 1, length);
+    }
+}
 
 int main(){
     char *s1 = "nick", *s2 = "paul", *s3 = "nick";
+    char *s4 = "khhghks";
     map_inc(s1);
     map_set(s2,1000);
     map_inc(s3);
-
+    map_set(s4, 42);
+    
     printf("%d\n",map_get(s1));
     printf("%d\n",map_get(s2));
     printf("%d\n",map_get(s3));
+    printf("%d\n",map_get(s4));
+
+    int length = 5;
+    char str[length + 1];  // +1 for the null terminator
+    generate_strings(str, 0, length);
+    printf("%s", str);
 
     return 0;
 }
