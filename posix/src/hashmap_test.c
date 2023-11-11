5c1b3a9b306e68ae342c768b3b6c0a25c3cbd0b8
paulniziolek <niziolek.paul@gmail.com>
2023-11-10 22:43:05 -0500

    Added more hashmap testing assertions

--- posix/src/hashmap_test.c
+++ posix/src/hashmap_test.c
@@ -21,24 +21,32 @@ void generate_strings(char *str, int index, int length) {
     }
 }
 
+void generate_strings_for_assertion(char *str, int index, int length) {
+    if (index == length) {
+        str[index] = '\0'; 
+
+        assert(mapGet(map, str) == global_ctr);
+        global_ctr++;
+        return;
+    }
+
+    for (char c = 'a'; c <= 'z'; c++) {
+        str[index] = c;
+        generate_strings_for_assertion(str, index + 1, length);
+    }
+}
+
 int main(){
     map = initMap();
-    char *s1 = "nick", *s2 = "paul", *s3 = "nick";
-    char *s4 = "khhghks";
-    mapInc(map, s1);
-    mapSet(map, s2,1000);
-    mapInc(map, s3);
-    mapSet(map, s4, 42);
-    
-    printf("%d\n",mapGet(map, s1));
-    printf("%d\n",mapGet(map, s2));
-    printf("%d\n",mapGet(map, s3));
-    printf("%d\n",mapGet(map, s4));
 
-    int length = 5;
+    int length = 4;
     char str[length + 1];  // +1 for the null terminator
     generate_strings(str, 0, length);
     printf("%s\n", str);
+    printf("%lld\n", global_ctr);
+
+    global_ctr = 1;
+    generate_strings_for_assertion(str, 0, length);
     printf("%lld\n", global_ctr);
 
     mapDestroy(map);
