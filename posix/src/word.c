84ea927d73384dc62ebbe2c0fff2fba954fecb94
paulniziolek <niziolek.paul@gmail.com>
2023-11-10 19:31:24 -0500

    processFile logic without edge cases

--- posix/src/word.c
+++ posix/src/word.c
@@ -1,9 +1,13 @@
+#include <ctype.h>
+#include <dirent.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <dirent.h>
 #include "hashmap.h"
+
+#define BUFFER_SIZE 1024
+#define INIT_WORD_SIZE 256
 
 // PROTO
 void processEntity(HashMap* map, const char* arg);
@@ -76,10 +80,58 @@ void processFile(HashMap* map, const char* arg) {
         fprintf(stderr, "File %s failed to open", arg);
         return;
     }
+    // debugging only
+    //printf("Reached processFile() for file %s\n", arg);
 
-    // TODO: process files
-    printf("Reached processFile() for file %s\n", arg); // debugging only
+    size_t wordSize = INIT_WORD_SIZE;
+    size_t wordIndex = 0;
+    ssize_t bytesRead = 0;
+    char buffer[BUFFER_SIZE];
+    char* word = (char *)malloc(INIT_WORD_SIZE);
+    if (!word) {
+        fprintf(stderr, "Memory allocation for word failed");
+        exit(EXIT_FAILURE);
+    }
 
+    // 1. Keep reading file while bytesRead > 0;
+    // 2. While word is not followed by any separator, we exponentially increase word size with realloc() and read characters in
+    // 3. If word reaches a separator condition, we clean word (if needed, in cases such as double hyphen) and then add to hashmap
+    // 4. Once loop from (1) is finished, we add in the word (if remaining) to hashmap since no separator follows it.
+
+    while ((bytesRead = read(fd, buffer, sizeof(buffer))) > 0) {
+        for (ssize_t i = 0; i < bytesRead; i++) {
+            // check if - ' and other separators are included in isalnum()
+            if (isalnum(buffer[i]) || buffer[i] == '-') {
+                if (wordIndex >= wordSize - 1) {
+                    // reallocate word
+                    wordSize *= 2;
+                    char *temp = (char *)realloc(word, wordSize);
+                    if (!temp) {
+                        fprintf(stderr, "Memory reallocation for word failed");
+                    }
+                    word = temp;
+                }
+                word[wordIndex] = buffer[i];
+                wordIndex += 1;
+                continue;
+            }
+            if  (wordIndex > 0) {
+                word[wordIndex] = '\0';
+                mapInc(map, word);
+                wordIndex = 0;
+            }
+        }
+    }
+
+    if (wordIndex > 0) {
+        word[wordIndex] = '\0';
+        mapInc(map, word);
+    }
+    free(word);
+
+    if (close(fd) == -1) {
+        fprintf(stderr, "File %s was interrupted during close()", arg);
+    }
 }
 
 char* constructNewPath(const char* arg, const char* name) {
@@ -87,7 +139,7 @@ char* constructNewPath(const char* arg, const char* name) {
     int newPathLen = strlen(arg) + strlen(name) + 2;
     char* newPath = (char *)malloc(newPathLen * sizeof(char));
     if (newPath == NULL) {
-        fprintf(stderr, "Memory allocation for path failed");
+        fprintf(stderr, "Memory allocation for path %s/%s failed", arg, name);
         exit(EXIT_FAILURE);
     }
 
