63b27fba323081ec2a85504c5101ae6951c8a06d
paulniziolek <niziolek.paul@gmail.com>
2023-12-03 21:33:23 -0500

    pwd() impl

--- mysh/src/builtins.c
+++ mysh/src/builtins.c
@@ -1,3 +1,13 @@
 #include "builtins.h"
 
-void cd() {}
+void cd(const char* arg) {}
+
+void pwd(int fd) {
+    char* cwd = getcwd(NULL, 0);
+    if (cwd == NULL) {
+        perror("getcwd() error");
+        return;
+    }
+    dprintf(fd, "%s\n", cwd);
+    free(cwd);
+}
--- mysh/src/builtins.h
+++ mysh/src/builtins.h
@@ -1,6 +1,11 @@
 #ifndef BUILTINS_H
 #define BUILTINS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "unistd.h"
+
+void pwd(int fd);
 
 
 #endif
